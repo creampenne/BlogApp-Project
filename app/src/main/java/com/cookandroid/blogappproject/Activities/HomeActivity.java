6f199c27d582cb8a03ec86230744cb529a0ae7c5
creampenne <wonhee5757@gmail.com>
2022-05-19 09:13:36 +0900

    Firebase User Registration

---
+++ app/src/main/java/com/cookandroid/blogappproject/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.blogappproject.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cookandroid.blogappproject.R;
+
+public class  HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/cookandroid/blogappproject/Activities/RegisterActivity.java
+++ app/src/main/java/com/cookandroid/blogappproject/Activities/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -14,10 +15,24 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.cookandroid.blogappproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,10 +41,57 @@ public class RegisterActivity extends AppCompatActivity {
     Intent intent;
     Uri pickedImgUri;
 
+    private EditText userName, userEmail, userPassword, userPassword2;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // ini views
+        userName = findViewById(R.id.regName);
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPassword2 = findViewById(R.id.regPassword2);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+
+        // progressbar default : invisible
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+
+                final String name = userName.getText().toString();
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+
+                if (email.isEmpty() || name.isEmpty() || password.isEmpty() || password2.isEmpty() || !password.equals(password2)) {
+
+                    // error
+                    showMessage("모든 항목을 입력해주세용");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+
+                }
+                else {
+                    // OK
+                    CreateUserAccount(email, name, password);
+                }
+
+            }
+        });
 
         ImgUserPhoto = findViewById(R.id.regUserPhoto);
 
@@ -40,11 +102,98 @@ public class RegisterActivity extends AppCompatActivity {
 
                 checkAndRequestForPermission();
 
-            } else {
+            }
+            else {
                 openGallery();
             }
 
         });
+
+    }
+
+    // create user account
+    private void CreateUserAccount(String email, String name, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            // user account created successfully
+                            showMessage("계정을 생성했습니당");
+                            // update name, picture
+                            updateUserInfo( name, pickedImgUri, mAuth.getCurrentUser());
+
+                        }
+                        else {
+                            // account creation failed
+                            showMessage("계정 생성을 실패했습니당\n" + task.getException().getMessage());
+                            regBtn.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+    }
+
+    // update name, picture
+    private void updateUserInfo(final String name, Uri pickedImgUri, final FirebaseUser currentUser) {
+
+        // upload picture to firebase storage, get url
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                // picture uploaded successfully, get picture url
+
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        if (task.isSuccessful()) {
+
+                                            // user info updated successfully
+                                            showMessage("회원가입 완료!");
+                                            updateUI();
+
+                                        }
+
+                                    }
+                                });
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    private void updateUI() {
+
+        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+
+    }
+
+    // simple method to show toast message
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 
     }
 
@@ -57,7 +206,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     // Check Permission
-    // TODO : 권한 부여되지 않았을 때 재요청
+    // TODO: 권한 부여되지 않았을 때 재요청
     private void checkAndRequestForPermission() {
 
         if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
