4159d76d977a87fcc69b5a2bf0973b2362b71c49
creampenne <wonhee5757@gmail.com>
2022-05-19 16:28:45 +0900

    Add activity_login.xml, Edit activity_register.xml

--- app/src/main/java/com/cookandroid/blogappproject/Activities/LoginActivity.java
+++ app/src/main/java/com/cookandroid/blogappproject/Activities/LoginActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.cookandroid.blogappproject.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
     }
 }
