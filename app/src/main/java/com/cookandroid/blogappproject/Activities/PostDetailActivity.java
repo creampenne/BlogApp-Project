dd3b0501a52d39f26aac12e87c3970c6d10fe0e2
creampenne <wonhee5757@gmail.com>
2022-06-03 13:59:33 +0900

    activity_post_detail.xml Design

---
+++ app/src/main/java/com/cookandroid/blogappproject/Activities/PostDetailActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.blogappproject.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cookandroid.blogappproject.R;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+    }
+}
